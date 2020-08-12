7dff68e48b16af214b1bd2739cb7098f9ffbe591
liunian <3051796096@qq.com>
2020-08-12 16:31:42 +0800

    后台

---
+++ app/Admin/routes.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Routing\Router;
+
+Admin::routes();
+
+Route::group([
+    'prefix'        => config('admin.route.prefix'),
+    'namespace'     => config('admin.route.namespace'),
+    'middleware'    => config('admin.route.middleware'),
+    'as'            => config('admin.route.prefix') . '.',
+], function (Router $router) {
+    $router->get('/', 'HomeController@index')->name('home');
+    $router->resource('goods-models', GoodsController::class);
+    $router->resource('order-models', OrderController::class);
+    $router->resource('video-models', VideoController::class);
+});
